48d6bd42439c2f66db328deaf7370c4461b826d6
Alex <simmonsjd1997@gmail.com>
2018-09-16 14:47:23 +1200

    Migrations added, seed data in separate .txt file

---
+++ StudentCourseApp/StudentCourseApp/Migrations/20180916015153_Initial.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace StudentCourseApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "char(4)", nullable: false),
+                    CName = table.Column<string>(unicode: false, maxLength: 40, nullable: true),
+                    Compulsory = table.Column<string>(type: "char(1)", nullable: false),
+                    Desc = table.Column<string>(type: "text", nullable: true),
+                    PreReq = table.Column<string>(type: "text", nullable: true),
+                    Semester = table.Column<string>(type: "char(1)", nullable: false),
+                    YearLevel = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(unicode: false, maxLength: 50, nullable: true),
+                    Phone = table.Column<string>(unicode: false, maxLength: 12, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enrollment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    CourseId = table.Column<string>(type: "char(4)", nullable: true),
+                    SId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrollment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CourseEnrol",
+                        column: x => x.CourseId,
+                        principalTable: "Course",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_StdEnrol",
+                        column: x => x.SId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollment_CourseId",
+                table: "Enrollment",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollment_SId",
+                table: "Enrollment",
+                column: "SId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enrollment");
+
+            migrationBuilder.DropTable(
+                name: "Course");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
