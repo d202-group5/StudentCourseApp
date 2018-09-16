17111009ad17b4715b318b7dbc15bf6a4daaa65a
Alex <simmonsjd1997@gmail.com>
2018-09-16 14:55:53 +1200

    Tidied up layout for Courses/Index.html

--- StudentCourseApp/StudentCourseApp/Models/Course.cs
+++ StudentCourseApp/StudentCourseApp/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; 
 
 namespace StudentCourseApp.Models
 {
@@ -10,12 +11,19 @@ namespace StudentCourseApp.Models
             Enrollment = new HashSet<Enrollment>();
         }
 
+        [Display(Name = "Course ID")]
         public string Id { get; set; }
+        [Display(Name = "Course Name")]
         public string Cname { get; set; }
+        [Display(Name = "Course Description")]
         public string Desc { get; set; }
+        [Display(Name = "Semester Offered")]
         public string Semester { get; set; }
+        [Display(Name = "Pre-requisites")]
         public string PreReq { get; set; }
+
         public string Compulsory { get; set; }
+        [Display(Name = "Year Level")]
         public int YearLevel { get; set; }
 
         public ICollection<Enrollment> Enrollment { get; set; }
